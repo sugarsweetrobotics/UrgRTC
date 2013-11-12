ef4e7ae9b6a3cc002a26d59452b00bfa51b758df
ysuga <ysuga@ysuga.net>
2013-11-12 16:51:31 +0900

    update for osx10.9

--- include/UrgRTC/UrgRTC.h
+++ include/UrgRTC/UrgRTC.h
@@ -10,14 +10,14 @@
 #ifndef URGRTC_H
 #define URGRTC_H
 
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
 #include <rtm/Manager.h>
 #include <rtm/DataFlowComponentBase.h>
 #include <rtm/CorbaPort.h>
 #include <rtm/DataInPort.h>
 #include <rtm/DataOutPort.h>
-#include <rtm/idl/BasicDataTypeSkel.h>
-#include <rtm/idl/ExtendedDataTypesSkel.h>
-#include <rtm/idl/InterfaceDataTypesSkel.h>
 
 // Service implementation headers
 // <rtc-template block="service_impl_h">
